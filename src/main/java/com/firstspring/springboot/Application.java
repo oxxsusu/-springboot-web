62395016928d552d0f1c91adcbbfa1c6942a5568
codesusuzz <hluvee@naver.com>
2022-07-17 04:05:37 +0900

    글 수정, 조회 기능 구현

--- src/main/java/com/firstspring/springboot/Application.java
+++ src/main/java/com/firstspring/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); // 내장 WAS 실행구문
-        /* 스프링 부트에서는 내장 WAS를 사용하는 것을 권장하는데,
+        /* 스프링 부트에서는 내장 WAS 를 사용하는 것을 권장하는데,
            언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있기 때문이다 */
     }
 }
--- src/main/java/com/firstspring/springboot/service/posts/PostsService.java
+++ src/main/java/com/firstspring/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.firstspring.springboot.service.posts;
 
+import com.firstspring.springboot.domain.posts.Posts;
 import com.firstspring.springboot.domain.posts.PostsRepository;
+import com.firstspring.springboot.web.dto.PostsResponseDto;
 import com.firstspring.springboot.web.dto.PostsSaveRequestDto;
+import com.firstspring.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("오류가 발생했습니다. 게시글 id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("오류가 발생했습니다. 게시글 id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
