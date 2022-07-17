0242b60d922c2d55ae7e752caabcf798520c4126
codesusuzz <hluvee@naver.com>
2022-07-17 16:04:23 +0900

    IndexController, 부트스트랩 추가

---
+++ src/main/java/com/firstspring/springboot/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.firstspring.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+        /*
+         * 머스테치 스타터가 있어서 컨트롤러에서 index 라는 문자열을 반환할 때
+           앞의 경로와 뒤의 파일 확장자가 자동으로 지정된다.
+         * src/main/resources/templates/index.mustache 의 경로로 전환되어
+           ViewResolver 라는 URL 요청 관리자가 알아서 처리한다.
+         */
+    }
+}
